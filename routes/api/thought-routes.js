ad7263d2ba2c70db7e8b1dd98d242876e51a3605
jacob m <jacobamaynard@gmail.com>
2023-03-10 00:44:43 -0600

    updated thought route and added thought controller

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 const {
     getThoughts,
     getSingleThought,
+    createThought,
     
-}
+} = require('../../controllers/thought-controller')
 
 router.route('/')
+    .get(getThoughts)
+    .post(createThought)
+
+router.route('/:thoughtId')
+    .get(getSingleThought)
+
+module.exports = router;
