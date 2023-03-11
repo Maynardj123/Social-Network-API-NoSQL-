1c568a87964644d605285ddb78248c4e827fa572
jacob m <jacobamaynard@gmail.com>
2023-03-10 18:35:46 -0600

    dont know what i did to the server.js, but fixed it

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require mode
-const { reactionSchema, Thought, User } = require('./models');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
 const routes = require("./routes")
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
-
-const PORT = 3001;
+// Require mode
+// const { reactionSchema, Thought, User } = require('./models');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 db.once('open', () => {
@@ -36,3 +15,19 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+// const express = require('express');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const PORT = 3001;
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//   });
+// });
