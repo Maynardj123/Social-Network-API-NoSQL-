af813bb846bd476081b8863306a2cad80d90887d
jacob m <jacobamaynard@gmail.com>
2023-03-10 18:18:45 -0600

    added comments and added update thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,13 @@
 const { User ,Thought} = require('../models')
 
 module.exports = {
+    // this will grab all the current thoughts and display them
     getThoughts(req,res) {
         Thought.find()
             .then((thought) => res.json(thought))
             .catch((err) => res.status(500).json(err))
     },
+    // this will grab a single thought and display it
     getSingleThought(req,res) {
         Thought.findOne({_id: req.params.thoughtId})
             .then((thought) =>
@@ -15,6 +17,7 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err))
     },
+    // used to create a new thought 
     createThought(req,res) {
         Thought.create(req.body)
             .then((thought) => {
@@ -34,9 +37,22 @@ module.exports = {
                 .catch((err) => {
                     console.error(err);
                 })
-    }
+    },
+    // Update a single thought so it contains new information
+    updateThought(req,res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId},
+            { $set: req.body},
+            {runValidators: true, new: true}
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this id'})
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err))
 }
 
-// thought update
 
 // thought delete
+}
